9286fcf173586906152ca5e4cc0f51bcf3f3cbb6
Giuseppe Badagliacca <joebadagliacca@gmail.com>
2022-05-11 21:49:47 -0400

    Created new branch with major chnages to JS.

--- app.js
+++ app.js
@@ -21,36 +21,32 @@ function runGame(e){
   }
 
   if(playerChoice === compChoice){
-    displayTie(`Both players picked ${playerChoice}...It'a tie :/`)
-  }else if(playerChoice === 'rock' &&compChoice === 'paper'){
-    const result = `${playerChoice} vs. ${compChoice}
-    Paper beats Rock...Computer wins :(`
-    addPointComp(result)
+    displayTie(`Both players picked ${playerChoice} ...It'a tie :/`)
+  }else if(playerChoice === 'rock' && compChoice === 'paper'){
+    displayChoiceRed(playerChoice)
+    playerChoiceDisplay(compChoice)
+    addPointComp(`Paper beats Rock...Computer wins :(`)
   }
-  else if(playerChoice === 'rock' &&compChoice === 'scissor'){
-    const result = `${playerChoice} vs. ${compChoice}
-
-    Rock beats Scissor...Player wins :)`
-    addPointPlayer(result)
-  }else if(playerChoice === 'paper' &&compChoice === 'rock'){
-    const result = `${playerChoice} vs. ${compChoice}
-
-    Paper beats Rock...Player wins :)`
-    addPointPlayer(result)
-  } else if(playerChoice === 'paper' &&compChoice === 'scissor'){
-    const result = `${playerChoice} vs. ${compChoice}
-
-    Scissor beats Paper...Computer wins :(`
-    addPointComp(result)
-  } else if(playerChoice === 'scissor' &&compChoice === 'rock'){
-    const result = `${playerChoice} vs. ${compChoice}
-
-    Rock Beats Scissor...Computer wins :(`
-    addPointComp(result)
+  else if(playerChoice === 'rock' && compChoice === 'scissor'){
+    displayChoiceRed(compChoice)
+    playerChoiceDisplay(playerChoice)
+    addPointPlayer(`Rock beats Scissor...Player wins!`)
+  }else if(playerChoice === 'paper' && compChoice === 'rock'){
+    displayChoiceRed(compChoice)
+    playerChoiceDisplay(playerChoice)
+    addPointPlayer(`Paper beats Rock...Player wins!`)
+  } else if(playerChoice === 'paper' && compChoice === 'scissor'){
+    displayChoiceRed(playerChoice)
+    playerChoiceDisplay(compChoice)
+    addPointComp(`Scissor beats Paper...Computer wins :(`)
+  } else if(playerChoice === 'scissor' && compChoice === 'rock'){
+    displayChoiceRed(playerChoice)
+    playerChoiceDisplay(compChoice)
+    addPointComp(`Rock Beats Scissor...Computer wins :(`)
   }else if(playerChoice === 'scissor' && compChoice === 'paper'){
-    const result = `${playerChoice} vs. ${compChoice}
-    Scissor beats Paper...Player wins :)`
-    addPointPlayer(result)
+    displayChoiceRed(compChoice)
+    playerChoiceDisplay(playerChoice)
+    addPointPlayer(`Scissor beats Paper...Player wins!`)
   }
 }
 
@@ -59,35 +55,50 @@ function displayTie(msg){
   compDisp.id = 'tie'
   compDisp.appendChild(document.createTextNode(msg))
   scoreBoard.appendChild(compDisp)
-  setTimeout(clear('tie'), 3500)
+  clearStatus('tie')
 }
 
 function addPointComp(x){
  const newScore = compScore + 1
  document.getElementById('comp-score').innerHTML = `${newScore}`
-
  const compDisp = document.createElement('div')
  compDisp.id = 'lose'
  compDisp.appendChild(document.createTextNode(`${x}`))
  scoreBoard.appendChild(compDisp)
- setTimeout(clear('lose'), 3500)
+ clearStatus('lose')
  return compScore = newScore
 }
 
 function addPointPlayer(x){
   const newScore = playerScore + 1
   document.getElementById('player-score').innerHTML = `${newScore}`
-
-const compDisp = document.createElement('div')
+  const compDisp = document.createElement('div')
  compDisp.id = 'win'
  compDisp.appendChild(document.createTextNode(`${x}`))
  scoreBoard.appendChild(compDisp)
- setTimeout(clear('win'), 3500)
-  return playerScore = newScore
+ clearStatus('win')
+ return playerScore = newScore
 }
 
-function clear(status){
-  // const element = document.querySelector(`#${status}`)
-  // element.remove()
+function clearStatus(status){
+  const element = document.querySelector(`#${status}`)
+  setTimeout((status)=>{
+    element.remove()
+  }, 4000)
 }
 
+function displayChoiceRed(choice){
+newChoice = document.querySelector(`img#${choice}`)
+newChoice.id = 'redBorder'
+clearImgBorder(choice, newChoice)
+}
+
+function playerChoiceDisplay(choice){
+  newChoice = document.querySelector(`img#${choice}`)
+  newChoice.id = 'greenBorder'
+  clearImgBorder(choice, newChoice)
+}
+
+function clearImgBorder(choice, newChoice){
+  setTimeout(()=>newChoice.id = `${choice}`, 4000)
+}
